ad0ed51b62c06252aeacf6e019475c89f2b9f9e9
gergelyviktor <gergelyv@kkszki.hu>
2025-02-26 12:06:04 +0100

    Add project files.

---
+++ GergelyViktor0226/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GergelyViktor0226
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
