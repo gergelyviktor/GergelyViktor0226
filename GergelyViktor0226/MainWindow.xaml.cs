e75862c2a581b374cb2c50c0b96851a06c7821dd
gergelyviktor <gergelyv@kkszki.hu>
2025-02-26 12:25:29 +0100

    Fájl hozzáadva, osztály megvan

--- GergelyViktor0226/MainWindow.xaml.cs
+++ GergelyViktor0226/MainWindow.xaml.cs
@@ -19,9 +19,17 @@ namespace GergelyViktor0226
         public MainWindow()
         {
             InitializeComponent();
+            // 1. feladat - beolvasás
+
+            // 2. feladat - az utolsó mérést kiírni a label1-be
+
+            // 3. feladat - az első mérésben szereplő rendszámhoz tartozó összes adatot kell kiírni a label2-be (+=)
+
+            // 5. feladat - fájlba kell írni a méréseket
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
+            // 4. feladat
 
         }
     }
---
+++ GergelyViktor0226/Meres.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GergelyViktor0226 {
+    internal class Meres {
+        public required string Rendszam { get; set; }
+        public int Ora { get; set; }
+        public int Perc { get; set; }
+        public int Sebesseg { get; set; }
+
+        public Meres(string rendszam, int ora, int perc, int sebesseg) {
+            Rendszam = rendszam;
+            Ora = ora;
+            Perc = perc;
+            Sebesseg = sebesseg;
+        }
+    }
+}
